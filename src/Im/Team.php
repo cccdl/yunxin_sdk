d226240c9bf4a04a795ad2fa5377e9a0c1ab7213
dl_c <568078220@qq.com>
2021-08-05 15:31:38 +0800

    编辑群资料

--- src/Im/Team.php
+++ src/Im/Team.php
@@ -162,4 +162,38 @@ class Team extends Base
 
         return $this->post('team/remove.action', $data);
     }
+
+    /**
+     * 编辑群资料
+     * 高级群基本信息修改
+     * tname           : String , 否	群名称，最大长度64字符
+     * announcement    : String , 否	群公告，最大长度1024字符
+     * intro           : String , 否	群描述，最大长度512字符
+     * joinmode        : int    , 否	群建好后，sdk操作时，0不用验证，1需要验证, 2不允许任何人加入。其它返回414
+     * custom          : String , 否	自定义高级群扩展属性，第三方可以跟据此属性自定义扩展自己的群属性。（建议为json） , 最大长度1024字符
+     * icon            : String , 否	群头像，最大长度1024字符
+     * beinvitemode    : int    , 否	被邀请人同意方式，0-需要同意(默认), 1-不需要同意。其它返回414
+     * invitemode      : int    , 否	谁可以邀请他人入群，0-管理员(默认), 1-所有人。其它返回414
+     * uptinfomode     : int    , 否	谁可以修改群资料，0-管理员(默认), 1-所有人。其它返回414
+     * upcustommode    : int    , 否	谁可以更新群自定义属性，0-管理员(默认), 1-所有人。其它返回414
+     * teamMemberLimit : int    , 否	该群最大人数(包含群主)，范围：2至应用定义的最大群人数(默认: 200)。其它返回414
+     * attach          : String , 否	自定义扩展字段，最大长度512
+     * @param string $tid 网易云信服务器产生，群唯一标识，创建群时会返回，最大长度128字符
+     * @param string $owner 群主用户帐号，最大长度32字符
+     * @param array  $options 上述非必填参数构建的数组
+     * @return array
+     * @throws GuzzleException
+     * @throws cccdlException
+     */
+    public function update(string $tid, string $owner, array $options = []): array
+    {
+        $data = [
+            'tid' => $tid,
+            'owner' => $owner,
+        ];
+
+        $data = array_merge($data, $options);
+
+        return $this->post('team/update.action', $data);
+    }
 }
